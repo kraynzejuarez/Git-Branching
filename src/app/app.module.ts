2a5cfc02a8ebb7a0bf56a0a0793f832166f35184
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-01-06 13:56:47 +0800

    app.module.ts v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
+/*Angular Imports*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/*App Import*/
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
 import { HighlightDirective } from './highlight.directive';
-import { ContactModule }      from './contact/contact.module';
+
+/* Routing Module*/
+import { AppRoutingModule }   from './app-routing.module';
+
 
 @NgModule({
-    imports:      [ BrowserModule, ContactModule ],
+    imports:      [ BrowserModule, AppRoutingModule ],
   declarations: [ AppComponent, HighlightDirective, TitleComponent ],
   providers:    [ UserService ],
   bootstrap:    [ AppComponent ],
   ],
-  imports: [
-    BrowserModule, Formsodule
-
-  ],
-  providers: [ContactService, UserService],
-  bootstrap: [AppComponent
-  ], 
   
 })
 export class AppModule { }
