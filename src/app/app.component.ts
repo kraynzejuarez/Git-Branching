1a9d1502e09eda000e8e0eb62355a2d18f0d44ec
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-01-06 13:49:17 +0800

    app.component.ts v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 
-  '<app-title></app-title>
-  <app-contact></app-contact>
-  '
+ template: `
+  <app-title></app-title>
+  <nav>
+    <a routerLink="contact" routerLinkActive="active">Contact</a>
+    <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
+    <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`
 })
 export class AppComponent {}
